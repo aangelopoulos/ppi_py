07f3553cc182760976ef3b8e49ce09517fcc1298
Anastasios N. Angelopoulos <angelopoulos@berkeley.edu>
2023-09-03 17:39:58 -0700

    [datasets downloading partial]

--- ppi_py/datasets/datasets.py
+++ ppi_py/datasets/datasets.py
@@ -1,3 +1,5 @@
+import os, sys
+import numpy as np
 """
     datasets.py
 
@@ -10,3 +12,21 @@
         # Make the dataset into a dataframe
         # Return the dataframe
 """
+
+def load_dataset(dataset_folder, dataset_name, download=True):
+    dataset_google_drive_ids = {
+            "alphafold" : "1lOhdSJEcFbZmcIoqmlLxo3LgLG1KqPho",
+            "ballots": "1DJvTWvPM6zQD0V4yGH1O7DL3kfnTE06u",
+            "census": None,
+            "forest": None,
+            "galaxy": None,
+            "gene_expression": None,
+            "plankton": None
+    }
+    if dataset_name not in dataset_google_drive_ids.keys():
+        raise NotImplementedError(f"The dataset {dataset_name} is not implemented. Valid options are {list(dataset_goole_drive_ids.keys())}.")
+    dataset_path = os.path.join(dataset_folder, dataset_name, ".npz")
+    if not os.path.exists(dataset_path):
+        print(f"Dataset {dataset_name} not found at location {dataset_folder}; downloading now...")
+        os.system(f"gdown {dataset_google_drive_ids[dataset_name]} -O {dataset_path}")
+    return np.load(dataset_path)
