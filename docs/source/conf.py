a15eda5c5b13ec9ea2d4fb2ec19c11d6c77ec2eb
GitHub Action <action@github.com>
2023-10-01 17:53:29 +0000

    Auto-format with Black

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,7 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+
+sys.path.insert(0, os.path.abspath("../.."))
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
