e55b8075e4996a21f6c46ae1b7a737ded7c20ee7
Anastasios N. Angelopoulos <angelopoulos@berkeley.edu>
2023-10-01 18:41:30 -0700

    [stable PyPI release]

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
-this_directory = Path(__file__).parent
-long_description = (this_directory / "assets/PyPI_README.md").read_text()
+long_description = open('./assets/PyPI-README.md').read()
 
 setup(
     name="ppi-python",
-    version="0.1.3",
+    version="0.1.4",
     packages=find_packages(),
+    include_package_data=True,
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=[
