84b73cf353a71f19393554b37888f0de099eb2d7
Anastasios N. Angelopoulos <angelopoulos@berkeley.edu>
2023-10-01 18:45:21 -0700

    [add description]

--- setup.py
+++ setup.py
@@ -5,9 +5,10 @@ long_description = open('./assets/PyPI-README.md').read()
 
 setup(
     name="ppi-python",
-    version="0.1.4",
+    version="0.1.5",
     packages=find_packages(),
     include_package_data=True,
+    description="Prediction-Powered Inference",
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=[
