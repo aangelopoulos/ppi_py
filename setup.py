8440ec360a4421a78a2d5edc5f25432d593670a5
GitHub Action <action@github.com>
2023-10-02 00:44:45 +0000

    Auto-format with Black

--- setup.py
+++ setup.py
@@ -4,5 +4,11 @@ setup(
     name="ppi-python",
     version="0.1.1",
     packages=find_packages(),
-    install_requires=["numpy", "pandas", "statsmodels", "scikit-learn", "gdown"],
+    install_requires=[
+        "numpy",
+        "pandas",
+        "statsmodels",
+        "scikit-learn",
+        "gdown",
+    ],
 )
